5659998a4fe1680e2b45983102a7e8b6a046dffb
3nnui <lex@immerda.ch>
2023-03-15 16:46:57 +0100

    security headers

--- site/config/config.php
+++ site/config/config.php
@@ -1,5 +1,16 @@
 <?php
 
 return [
-  'debug' => true
+  'debug' => true,
+  'bnomei.securityheaders.headers' => [
+    "X-Powered-By" => "", // unset
+    "X-Frame-Options" => "SAMEORIGIN",
+    "X-XSS-Protection" => "1; mode=block",
+    "X-Content-Type-Options" => "nosniff",
+    "strict-transport-security" => "max-age=31536000; includeSubdomains",
+    "Referrer-Policy" => "no-referrer-when-downgrade",
+    "Permissions-Policy" => 'interest-cohort=()', // flock-off,
+    // ... FEATURE POLICIES
+    // other options...
+  ]
 ];
