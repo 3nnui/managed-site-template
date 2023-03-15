d1743d0b362227a4c65f0f9433f6be617b73c7c9
3nnui <lex@immerda.ch>
2023-03-15 16:05:46 +0100

    content block fieldset

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,4 +1,4 @@
 <?php snippet('header', ['entry' => 'home']); ?>
 <h1 class="text-4xl"><?= $page->title() ?></h1>
-<p><?= $page->text()->html() ?></p>
+<p><?= $page->text()->markdown() ?></p>
 <?php snippet('footer', ['entry' => 'home']); ?>
