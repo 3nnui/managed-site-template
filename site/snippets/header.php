8156d7eee02613445a06bc503df60264259ba661
3nnui <lex@immerda.ch>
2023-03-21 13:36:48 +0100

    block content

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -7,7 +7,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Kirby Vite Multi-Page</title>
+  <title><?=$site->title()->text() ?></title>
   <!-- Include the shared css ... -->
   <?= vite()->css() ?>
   <!-- ... and the template's css (if it exists) -->
--- site/templates/home.php
+++ site/templates/home.php
@@ -1,6 +1,6 @@
 <?php snippet('header', ['entry' => 'home']); ?>
 <main>
-  <h1><?= $page->title() ?></h1>
-  <p><?= $page->text()->markdown() ?></p>
+  <h1><?= $site->title() ?></h1>
+  <p><?= $page->layout()->toBlocks() ?></p>
 </main>
 <?php snippet('footer', ['entry' => 'home']); ?>
