2397ec0102a50e0bc46667dfba87899cdb72975a
3nnui <lex@immerda.ch>
2023-03-21 13:39:57 +0100

    custom block plugins

---
+++ site/plugins/custom/blocks/snippets/blocks/linklist.php
@@ -0,0 +1,18 @@
+<ul class="linklist">
+  <?php
+  $rows = $block->rows()->toStructure();
+
+  foreach ($rows as $row): ?>
+    <?php if ($link = $row->link()->toValidLink()) : ?>
+      <li>
+        <a href="<?= $link->href() ?>" target="<?= $link->popup() ?>">
+          <?= $link->text() ? : $link->href() ?>
+        </a>
+      </li>
+    <?php else: ?>
+      <li>
+        <p class="error">Invalid Link! Is the url field empty?</p>
+      </li>
+    <?php endif ?>
+  <?php endforeach ?>
+</ul>
